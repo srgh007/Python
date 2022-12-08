1e035af0d8b7a32f28659922a197ffff714c53bb
srgh <srgh@bk.ru>
2022-12-08 15:55:45 +0300

    dbdict

--- Test/Untitled-2.py
+++ Test/Untitled-2.py
@@ -1,16 +1,18 @@
 import psycopg2
+import psycopg2.extras
 
 conn = psycopg2.connect(
     dbname="strength", user="postgres", password="postgres", host="localhost"
 )
 
-cursor = conn.cursor()
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 cursor.execute("SELECT * FROM tasks LIMIT 10")
+
 records = cursor.fetchall()
 
 for row in records:
-    print(row)
+    print(row["title"])
 
 cursor.close()
 conn.close()
