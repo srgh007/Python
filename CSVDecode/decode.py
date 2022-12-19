88b07d0bea4891215d8427bdd62c187e503c9b5e
srgh <srgh@bk.ru>
2022-12-20 00:12:46 +0300

    Decoder - add SQL connect (ODBC)

--- CSVDecode/decode.py
+++ CSVDecode/decode.py
@@ -1,6 +1,20 @@
 import csv
 import locale
 
+# import pyodbc
+
+# cnxn = pyodbc.connect(
+#     "Driver={SQL Server Native Client 11.0};"
+#     "Server=server_name;"
+#     "Database=db_name;"
+#     "Trusted_Connection=yes;"
+# )
+# cursor = cnxn.cursor()
+# cursor.execute("SELECT * FROM Table")
+
+# for row in cursor:
+#     print("row = %r" % (row,))
+
 locale.setlocale(locale.LC_ALL, "ru_RU")
 csvlist = {}
 maplist = {}
