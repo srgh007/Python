48175e215fbd4bff66d8c11985d402ade483d2d7
srgh <srgh@bk.ru>
2022-12-20 23:52:14 +0300

    Clean decoder

--- CSVDecode/decode.py
+++ CSVDecode/decode.py
@@ -42,70 +42,15 @@ for (dirpath, dirnames, filenames) in walk("E:\\Python\\CSVDecode\\OUT"):
     outfile = filenames[0]
     break
 
-# tempFile = open(out, "w")
-# line = fileinput.input(tempFile)[0]
-
-# with open(out, "r") as file:
-#     filedata = file.read()
-
-
-# tempFile.write(line.replace(maps, maplist[maps]))
-# with fileinput.FileInput(out, inplace=True, backup=".bak") as file:
-# for line in file:
-# file[0].replace(maps, maplist[maps])
-
-
-# with open(out, "w") as file:
-#     file.write(filedata)
-
-# file.close()
-
-
-def replace_word(infile, old_word, new_word):
-    if not os.path.isfile(infile):
-        print("Error on replace_word, not a regular file: " + infile)
-        sys.exit(1)
-
-    f1 = open(infile, "r", encoding="UTF8").read()
-    f2 = open(infile, "w", encoding="UTF8")
-    m = f1.replace(old_word, new_word)
-    if new_word != "" or old_word != "":
-        f2.write(m)
-
-
 df = pandas.read_csv(out, delimiter=";")
 
-# print(df)
-
-# print(maps)
-# print(maplist[maps])
-# if maps == "":
 del maplist[""]
 
-# print(maplist)
-
-
-# replace_word(out, maps, maplist[maps])
-# df.rename(
-#     columns=maplist,
-#     inplace=True,
-# )
-# df.to_csv(sep=";")
-# type: ignore
-
-# df.set_index('Custom field (Verified Date)').to_csv(out, index=None)
-
-
-# def rename_column(old_col_name, new_col_name, optional_file=True):
 outfile = ""
 for (dirpath, dirnames, filenames) in walk("E:\\Python\\CSVDecode\\OUT"):
     outfile = os.path.join(dirpath, filenames[0])
 
 df = pandas.read_csv(outfile, sep=r";", index_col=False)
 
-
-# for maps in maplist:
 df.rename(columns=maplist, inplace=True)
 df.to_csv(outfile, sep=";", index=False)
-
-# rename_column(outfile, , )
