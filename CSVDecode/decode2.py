b09e3856bc29ebc55b58684bee3dc75bc4cc37d9
srgh <srgh@bk.ru>
2022-12-23 00:35:36 +0300

    Row from xls to list

--- CSVDecode/decode2.py
+++ CSVDecode/decode2.py
@@ -6,7 +6,12 @@ import sys
 from os import walk
 
 import pandas
+from openpyxl import load_workbook
 
+if not sys.warnoptions:
+    import warnings
+
+    warnings.simplefilter("ignore")
 # import pyodbc
 
 # cnxn = pyodbc.connect(
@@ -22,6 +27,48 @@ import pandas
 #     print("row = %r" % (row,))
 
 locale.setlocale(locale.LC_ALL, "ru_RU")
+
+outfile = ""
+for (dirpath, dirnames, filenames) in walk("E:\\Python\\CSVDecode\\IN"):
+    print(filenames)
+    for file in filenames:
+        if "xls" in file:
+            outfile = os.path.join(dirpath, file)
+            print(outfile)
+
+wb_form = load_workbook(filename=outfile)
+wb_val = load_workbook(filename=outfile, data_only=True)
+
+sheet_val = wb_val["Тело"]
+
+cell_vals = sheet_val["{}4".format(chr(65))].value
+print(cell_vals)
+
+# listprms = []
+
+
+def listOfparams():
+    listprms = []
+    for i in range(65, 90):
+        listprms.append(sheet_val["{}4".format(chr(i))].value)
+
+    for g in range(65, 66):
+        for i in range(65, 90):
+            if sheet_val["{}{}4".format(chr(g), chr(i))].value != None:
+                listprms.append(sheet_val["A{}4".format(chr(i))].value)
+            else:
+                return listprms
+
+    # for i in range(65, 90):
+    #     if sheet_val["B{}4".format(chr(i))].value != None:
+    #         listprms.append(sheet_val["A{}4".format(chr(i))].value)
+    #     else:
+    #         return listprms
+
+
+# print(listOfparams()[0])
+
+# type: ignore
 csvlist = {}
 maplist = {}
 with open("CSVDecode\\mapping.csv", encoding="UTF8") as csvfile:
